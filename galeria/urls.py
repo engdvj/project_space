30910722e93c678cb7e8691af215e970e2a1c208
engdvj <eng.dvj@gmail.com>
2024-08-25 23:32:55 -0300

    excluir comentarios

--- galeria/urls.py
+++ galeria/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from galeria.views import index, imagem
 
 urlpatterns = [
-    path('', index, name='index'),  # Corrigido 'nome' para 'name'
-    path('imagem/', imagem, name='imagem')  # Corrigido 'nome' para 'name'
+    path('', index, name='index'),  
+    path('imagem/', imagem, name='imagem') 
 ]
